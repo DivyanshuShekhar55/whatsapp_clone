a581957bde2dc7c06dd2b9732d4f81f4e1cb36c2
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-05-30 18:17:57 +0530

    Chat component added

--- app/src/main/java/com/skywalker/whatsapp_clone/presentation/homescreen/ChatComponent.kt
+++ app/src/main/java/com/skywalker/whatsapp_clone/presentation/homescreen/ChatComponent.kt
@@ -2,29 +2,74 @@ package com.skywalker.whatsapp_clone.presentation.homescreen
 
 import android.graphics.drawable.shapes.OvalShape
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.skywalker.whatsapp_clone.R
 
 @Composable
 @Preview
-fun ChatComponent(){
-    Row(modifier = Modifier.padding(8.dp)) {
+fun ChatComponent() {
+    Row(modifier = Modifier.padding(8.dp), verticalAlignment = Alignment.CenterVertically) {
         Image(
             painter = painterResource(R.drawable.pfp_1),
             contentDescription = null,
-            modifier= Modifier
+            modifier = Modifier
                 .size(70.dp)
-                .clip(shape= CircleShape)
+                .clip(shape = CircleShape), contentScale = ContentScale.Crop
         )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Column(
+            modifier = Modifier
+                .padding(start = 12.dp)
+        ) {
+            Row (horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()){
+                Text(
+                    "Sakamoto Taro",
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 24.sp,
+                )
+
+                Text(
+                    "10:29 AM",
+                    fontSize = 12.sp,
+                    color = Color.Gray
+                )
+
+            }
+
+            Text(
+                "Wanna come Japan ??",
+                fontWeight = FontWeight.Normal,
+                fontSize = 18.sp,
+            )
+        }
+
     }
+
+    HorizontalDivider(thickness = 0.8.dp)
+
 }
