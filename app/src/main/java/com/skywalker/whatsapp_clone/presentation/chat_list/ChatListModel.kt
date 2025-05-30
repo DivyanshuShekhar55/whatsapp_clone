6237c2d3e3988678920fdf26001bfb27f8435739
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-05-30 18:46:36 +0530

    added model contents for chat list

--- app/src/main/java/com/skywalker/whatsapp_clone/presentation/chat_list/ChatListModel.kt
+++ app/src/main/java/com/skywalker/whatsapp_clone/presentation/chat_list/ChatListModel.kt
@@ -1,5 +1,8 @@
 package com.skywalker.whatsapp_clone.presentation.chat_list
 
 data class ChatListModel(
-
+    val image : Int,
+    val name : String,
+    val time : String,
+    val message : String
 )
