88aed797d4d257488def013668fda3135943d98e
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-05-31 17:41:36 +0530

    added status screen file

---
+++ app/src/main/java/com/skywalker/whatsapp_clone/presentation/status_screen/StatusScreen.kt
@@ -0,0 +1,10 @@
+package com.skywalker.whatsapp_clone.presentation.status_screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun StatusSCreen(){
+
+}
