e6e2d85853e11d643b65dfbe2318f07b886b6c6f
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-06-03 13:01:43 +0530

    comm. screen updates

--- app/src/main/java/com/skywalker/whatsapp_clone/presentation/communityscreen/CreateServerComponent.kt
+++ app/src/main/java/com/skywalker/whatsapp_clone/presentation/communityscreen/CreateServerComponent.kt
@@ -1,17 +1,31 @@
 package com.skywalker.whatsapp_clone.presentation.communityscreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.skywalker.whatsapp_clone.R
 
 @Composable
 fun CreateServerComponent() {
@@ -33,6 +47,32 @@ fun CreateServerComponent() {
             lineHeight = 20.sp,
             textAlign = TextAlign.Center
         )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .background(
+                    color = colorResource(R.color.light_green),
+                    shape = CircleShape
+                )
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp, vertical = 8.dp)
+
+        ) {
+
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Text(
+                "Create Own Community",
+                fontWeight = FontWeight.Medium,
+                fontSize = 18.sp,
+                color = Color.White
+            )
+        }
 
     }
 }
