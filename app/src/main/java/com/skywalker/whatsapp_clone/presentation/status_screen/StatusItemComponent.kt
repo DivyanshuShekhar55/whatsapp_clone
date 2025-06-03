1d1fa09039bb536d8fbe933de410173cf5439e2b
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2025-06-03 10:55:15 +0530

    finished with status item compo

--- app/src/main/java/com/skywalker/whatsapp_clone/presentation/status_screen/StatusItemComponent.kt
+++ app/src/main/java/com/skywalker/whatsapp_clone/presentation/status_screen/StatusItemComponent.kt
@@ -2,29 +2,49 @@ package com.skywalker.whatsapp_clone.presentation.status_screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.skywalker.whatsapp_clone.R
 
 @Composable
-fun StatusItem(){
-    Row {
-        Box(){
-            Image(
-                painter = painterResource(R.drawable.pfp_2),
-                "null",
-                modifier= Modifier
-                    .size(60.dp)
-                    .clip(CircleShape),
-                contentScale = ContentScale.Crop
-            )
+fun StatusItem() {
+    Row (
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Image(
+            painter = painterResource(R.drawable.pfp_2),
+            "null",
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Column {
+            Text("Sakamoto", fontWeight = FontWeight.SemiBold, fontSize = 18.sp)
+            Text("Just Now", color = Color.Gray)
         }
+
     }
 }
